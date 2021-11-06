8b63d8ea636046da803bd5308d3b9a007190bc2b
jhonatanMD <jhonatanmd1999@gmail.com>
2021-11-05 22:37:54 -0500

    'add'

---
+++ src/main/java/com/ws/MsMysqlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMysqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMysqlExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ws/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.ws.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ws")
+public class AppController {
+
+
+
+
+
+}
